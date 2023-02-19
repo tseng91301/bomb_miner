9a631caca1e9b2f7645f7fba915b03882dc57ca2
ChingKai Tseng <83930467+tseng91301@users.noreply.github.com>
2023-02-19 22:44:25 +0800

    2023-02-19

---
+++ not_important/transform.sh
@@ -0,0 +1,7 @@
+echo "# bomb_miner" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:tseng91301/bomb_miner.git
+git push -u origin main
